b33ab932aa813090b0548d99f4e538cf39616b01
Antônio Santana <antonionetosg6@gmail.com>
2023-03-03 01:29:09 -0300

    feat: requisito 6

--- src/Battle/Battle.ts
+++ src/Battle/Battle.ts
@@ -10,3 +10,14 @@
 // }
 
 // export default Battle;
+
+import Fighter from '../Fighter';
+
+abstract class Battle {
+  constructor(protected player: Fighter) { }
+  fight(): number {
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}
+
+export default Battle;
